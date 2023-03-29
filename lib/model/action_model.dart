c858bec434501393685a76dac34cb5415359d518
SovaNS <sovaster.n@mail.ru>
2023-03-29 11:29:17 +0300

    Initial commit

---
+++ lib/model/action_model.dart
@@ -0,0 +1,17 @@
+import 'package:conduit/conduit.dart';
+import 'package:dart_application_1/model/user.dart';
+
+class Action extends ManagedObject<_Action> implements _Action
+{
+
+}
+
+@Table(name: 'actions')
+class _Action {
+  @primaryKey
+  int? id;
+  @Column(unique: false)
+  String? title;
+  @Relate(#actions)
+  User? user;
+}
---
+++ lib/model/finance_operation.dart
@@ -0,0 +1,31 @@
+import 'package:conduit/conduit.dart';
+import 'package:dart_application_1/model/operation_category.dart';
+import 'package:dart_application_1/model/user.dart';
+
+class FinanceOperation extends ManagedObject<_FinanceOperation> implements _FinanceOperation
+{
+
+}
+
+@Table(name: "finance_operation")
+class _FinanceOperation
+{
+  @primaryKey
+  int? id;
+  @Column(unique: true, indexed: true)
+  String? number;
+  @Column(unique: false, indexed: true)
+  String? name;
+  @Column(unique: false, indexed: true)
+  String? description;
+  @Column(unique: false, indexed: true)
+  DateTime? executionDate;
+  @Column(unique: false, indexed: true)
+  double? totalSum;
+  @Relate(#financialOperations)
+  User? user;
+  @Relate(#financialOperations)
+  OperationCategory? financeOperationCategory;
+  @Column(defaultValue: 'false')
+  bool? deleted;
+}
